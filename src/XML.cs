dd21e7940fe6a1e6b0fed3670b58c9e0194a5c62
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-06-12 12:10:22 +0200

    added XML type

---
+++ src/XML.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Shell
+{
+  static class XML
+  {
+    static void WriteXML(List<Item> list, string strPath)
+    {
+      XmlSerializer x = new XmlSerializer(typeof(Item));
+      TextWriter writer = new StreamWriter(strPath);
+      x.Serialize(writer, list);
+    }
+
+    List<Item> ReadXML(string strPath)
+    {
+      XmlSerializer x = new XmlSerializer(typeof(Item));
+      TextReader reader = new StreamReader(strPath);
+
+      return x.Deserialize(reader);
+    }
+  }
+}
