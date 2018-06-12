a2ae38fb7abfdbbdc8cac447cb55da5c6b122d95
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-06-12 12:10:08 +0200

    Added Item.cs

---
+++ src/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Shell
+{
+  [XmlRoot("Item")]
+  class Item
+  {
+    [XmlElement("Name")]
+    public string Name { get; set; }
+    [XmlElement("Path")]
+    public string Path { get; set; }
+  }
+}
