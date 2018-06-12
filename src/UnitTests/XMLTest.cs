1dcde3225b8cc0b55d48b28f3dfcdb39268e3aaa
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-06-12 23:06:00 +0200

    Add XML test

--- src/UnitTests/XMLTest.cs
+++ src/UnitTests/XMLTest.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Shell;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests
 {
   [TestClass]
-  public class UnitTest1
+  public class XMLTest
   {
+    const string XMLPATH = "./Commands.xml";
     [TestMethod]
-    public void TestMethod1()
+    public void Generation()
     {
+      //Generation of XML
+      List<Item> lstItem = new List<Item>();
+      lstItem.Add(new Item() { Name = "test1", Path = "test1.exe" });
+      lstItem.Add(new Item() { Name = "test2", Path = "test2.exe" });
+      XML.Serialization(lstItem, XMLPATH);
+      //Read XML
+      List<Item> lstRead = XML.Deserialize(XMLPATH);
+      //Asserts
+      Assert.AreEqual(lstItem.Count, lstRead.Count,"Error in List.Count");
+      for (int i = 0; i < lstItem.Count; i++)
+      {
+        Assert.AreEqual(lstItem[i].Name, lstRead[i].Name,"Error in Name");
+        Assert.AreEqual(lstItem[i].Path, lstRead[i].Path,"Error in Path");
+      }
+
     }
   }
 }
