808856679deb3b346daf01915adfcf094f275d22
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-06-12 16:48:42 +0200

    Add comentaries

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 
 namespace Shell
 {
-  class Program
+  static class Program
   {
     static void Main(string[] args)
     {
@@ -17,7 +17,9 @@ namespace Shell
         strInput = Console.ReadLine();
         //Empty input
         if (string.IsNullOrWhiteSpace(strInput))
+        {
           continue;
+        }
         //Commands splited
         string[] strCommand = strInput.Split(' ');
         switch (strCommand[0].ToLower())
@@ -33,30 +35,46 @@ namespace Shell
             //Add Command joining the end of the array
             CommandMgr.AddCommand(strCommand[1], string.Join(" ", strCommand.ToList().Skip(2)));
             break;
+          //Remove a command
+          case "remove":
+            //Todo
+            break;
           //Execute
           default:
+            //Get the path of the command
             string strPath = CommandMgr.GetCommandPath(strCommand[0]);
+            //If the path is empty, continue
             if (string.IsNullOrWhiteSpace(strPath))
             {
               Console.WriteLine("Comando no registrado");
               continue;
             }
+            //Create the process
             Process commandProcess = new Process();
+            //Create the start info
             ProcessStartInfo startInfo = new ProcessStartInfo();
+            //Set the start path
             startInfo.FileName = strPath;
+            //Set the arguments
             startInfo.Arguments = /*"/c " + */string.Join(" ", strCommand.ToList().Skip(1));
-            startInfo.RedirectStandardOutput = true;            
+            //Redirecto de info streams
+            startInfo.RedirectStandardOutput = true;
             startInfo.RedirectStandardInput = true;
             startInfo.RedirectStandardError = true;
             startInfo.CreateNoWindow = true;
             startInfo.UseShellExecute = false;
+            //Set the start info
             commandProcess.StartInfo = startInfo;
+            //Suscribe the events
             commandProcess.OutputDataReceived += (s, e) => Console.WriteLine(e.Data);
             commandProcess.ErrorDataReceived += (s, e) => Console.WriteLine(e.Data);
+            //Start the process
             commandProcess.Start();
+            //Start the events firer
             commandProcess.BeginOutputReadLine();
             commandProcess.BeginErrorReadLine();
-            //commandProcess.WaitForExit();
+            //Wait for exit
+            commandProcess.WaitForExit();
             break;
         }
       }
