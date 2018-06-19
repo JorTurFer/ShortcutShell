cb45a2210ce25733fdb1f2de470f7b7aca8e8141
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-06-19 12:40:50 +0200

    Refactorized UnitaryTest

--- src/UnitTests/CommandMgrTest.cs
+++ src/UnitTests/CommandMgrTest.cs
@@ -20,14 +20,15 @@ namespace UnitTests
       var lstCommands = CommandMgr.GetCommands();
       Assert.AreEqual(0, lstCommands.Count, "Error cleaning the CommandMgr");
       TestContext.WriteLine($"List.Count After = {lstCommands.Count}");
-      CommandMgr.AddCommand("test1 -p \"test1.exe\"");
-      CommandMgr.AddCommand("test2 -p \"test2.exe\"");
+      CommandMgr.AddCommand(new Command { Name = "test1", Path = "test1.exe" });
+      CommandMgr.AddCommand(new Command { Name = "test2", Path = "test2.exe" });
       Assert.AreEqual(2, CommandMgr.GetCommands().Count, "Error adding commands");
       TestContext.WriteLine($"List.Count Before = {CommandMgr.GetCommands().Count}");
     }
     [TestMethod]
     public void PathByNameTest()
     {
+
       Assert.AreEqual("test1.exe", CommandMgr.GetCommandPathByName("test1"), "Error, getting the path");
       Assert.AreEqual("test2.exe", CommandMgr.GetCommandPathByName("test2"), "Error, getting the path");
       TestContext.WriteLine($"Expected \"test1.exe\" -> Recived \"{CommandMgr.GetCommandPathByName("test1")}\"");
@@ -47,8 +48,8 @@ namespace UnitTests
       var lstCommands = CommandMgr.GetCommands();
       Assert.AreEqual(2, lstCommands.Count, "Error, cleaned CommandMgr");
       TestContext.WriteLine($"List.Count After = {lstCommands.Count}");
-      CommandMgr.RemoveCommand("test1");
-      CommandMgr.RemoveCommand("test2");
+      CommandMgr.RemoveCommand(new Command { Name = "test1" });
+      CommandMgr.RemoveCommand(new Command { Name = "test2" });
       Assert.AreEqual(0, CommandMgr.GetCommands().Count, "Error removing commands");
       TestContext.WriteLine($"List.Count Before = {CommandMgr.GetCommands().Count}");
     }
